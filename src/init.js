ddee20194e5697ece076f37b7de2ea8d13fa1ce6
james green <jamesgreen.311@gmail.com>
2024-12-13 13:37:49 -0500

    Remove sensitive information

--- src/init.js
+++ src/init.js
@@ -1,6 +1,3 @@
-const LIBRARY_LINK =
-   "https://script.google.com/macros/library/d/1upLmjjJDRdwVwzr91tLpdA9ouZuNMn_X1AvP_MtiSunmWcxMXNC6lEkA/1" // Version 1
-
 function init() {
    // Scopes needed
    // SpreadsheetApp.open(file)
